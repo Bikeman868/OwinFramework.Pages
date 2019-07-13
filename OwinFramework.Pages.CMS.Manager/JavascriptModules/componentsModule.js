b4531edf5d59a3daf685649caf20e658f1015358
Martin Halliday <martin.bikeman@gmail.com>
2019-07-13 12:35:01 -0700

    Added a tab-dialog effect to the editors

--- OwinFramework.Pages.CMS.Manager/JavascriptModules/componentsModule.js
+++ OwinFramework.Pages.CMS.Manager/JavascriptModules/componentsModule.js
@@ -1,4 +1,46 @@
 ﻿exported.components = function() {
+    Vue.component("cms-tabs", {
+        template:
+            "<div>"+
+            "    <ul class=\"cms_tabs\">" +
+            "      <li v-for=\"tab in tabs\" :class=\"{ 'cms_is_active': tab.isActive }\">"+
+            "          <a :href=\"tab.href\" @click=\"selectTab(tab)\">{{ tab.name }}</a>"+
+            "      </li>"+
+            "    </ul>"+
+            "    <div class=\"cms_tabs_details\">"+
+            "        <slot></slot>"+
+            "    </div>"+
+            "</div>",
+        data: function() {
+            return { tabs: [] };
+        },
+        created: function() {
+            this.tabs = this.$children;
+        },
+        methods: {
+            selectTab: function(selectedTab) {
+                this.tabs.forEach(function(tab) { tab.isActive = (tab.name === selectedTab.name); });
+            }
+        }
+    });
+
+    Vue.component("cms-tab", {
+        template:"<div v-show=\"isActive\"><slot></slot></div>",
+        props: {
+            name: { required: true },
+            selected: { default: false}
+        },
+        data: function() {
+            return {
+                isActive: false
+            };
+        },
+        computed: {
+            href: function() { return "#" + this.name.toLowerCase().replace(/ /g, "-"); }
+        },
+        mounted: function() { this.isActive = this.selected; }
+    });
+
     Vue.component("cms-website-version-field-edior",
     {
         props: {

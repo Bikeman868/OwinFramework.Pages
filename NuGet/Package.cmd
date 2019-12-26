7be8eb9cecefbba83d4bdaabb059d2d56aa9a2e7
Martin Halliday <martin.bikeman@gmail.com>
2019-12-25 16:39:42 -0800

    Added nuspec files to replace deleted Paket target files

---
+++ NuGet/Package.cmd
@@ -0,0 +1,10 @@
+nuget.exe pack Owin.Framework.Pages.CMS.Manager.nuspec
+nuget.exe pack Owin.Framework.Pages.CMS.Runtime.nuspec
+nuget.exe pack Owin.Framework.Pages.Core.nuspec
+nuget.exe pack Owin.Framework.Pages.DebugMiddleware.nuspec
+nuget.exe pack Owin.Framework.Pages.Framework.nuspec
+nuget.exe pack Owin.Framework.Pages.Html.nuspec
+nuget.exe pack Owin.Framework.Pages.Mocks.nuspec
+nuget.exe pack Owin.Framework.Pages.Restful.nuspec
+nuget.exe pack Owin.Framework.Pages.Standard.nuspec
+pause
